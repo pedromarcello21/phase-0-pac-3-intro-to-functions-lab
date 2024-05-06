4ffbea22c1328fb2bfdbb874fb3554f05e79e591
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-05-06 18:36:34 -0400

    function practice

---
+++ index.js
@@ -0,0 +1,27 @@
+const shout = (string) => {
+    return string.toUpperCase();
+}
+console.log(shout('yo'))
+
+const whisper = (string) => {
+    return string.toLowerCase();
+}
+console.log(whisper('AHHHHH'))
+
+const logShout = (string) => {
+    console.log(string.toUpperCase());
+}
+
+const logWhisper = (string) => {
+    console.log(string.toLowerCase())
+}
+const sayHiToHeadphonedRoommate = (string) => {
+    if (string === string.toUpperCase()) {
+        return "YES INDEED!"
+    } else if (string === string.toLowerCase()) {
+        return "I can\'t hear you!"
+    } else if (string == "Let\'s have dinner together!") {
+        return "I would love to!"
+    }
+}
+
